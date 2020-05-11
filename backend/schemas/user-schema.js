bba1a8d926828ea6bd7db61ac6820042bfa74c33
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-11 12:31:38 -0700

    finished user-schema

---
+++ backend/schemas/user-schema.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    address: {type: String, required: true}
+});
+
+module.exports = mongoose.model('user', userSchema);

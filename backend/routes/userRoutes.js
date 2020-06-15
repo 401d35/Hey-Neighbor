4ca48b3452a0602a09a77f60eb2c13c55dce0f76
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-15 11:50:34 -0700

    Fix google OAuth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,31 +3,20 @@
 
 const express = require('express');
 const userRoutes = express.Router();
-const superagent = require('superagent');
 const userSchema = require('../schemas/user-schema.js');
 const Model = require('../schemas/model.js');
 const users = require('../schemas/user-model.js');
 const basicAuth = require('../auth/basic-auth.js');
 const itemSchema = require('../schemas/item-schema.js'); // can get rid of this later
 
-const oauth = require('../auth/google-oauth/google-oauth.js');
+const googleOAuth = require('../auth/google-oauth/google-oauth.js');
 
-userRoutes.post('/oauth', (req, res) => {
-  let token = req.body.id_token;
-  let otherTokenEndpoint = `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`;
-  superagent.get(otherTokenEndpoint)
-    .then(response => {
-      const userName = response.body.email;
-      const token = users.generateToken({ userName });
-      const newRecord = {
-        userName: response.body.email,
-        password: 'anything',
-        email: response.body.email,
-        address: 'google'
-      };
-      users.signup(newRecord);
-      res.status(200).send(token);
-    });
+userRoutes.get('/oauth', googleOAuth, (req, res) => {
+  try {
+    res.status(200).send(req.token);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 userRoutes.get('/user/name/:userName', async function (req, res) {
@@ -39,7 +28,7 @@ userRoutes.get('/user/name/:userName', async function (req, res) {
     if (dbUser.length === 1) {
       res.status(200).json(dbUser[0]);
     }
-  }catch(e){
+  } catch (e) {
     res.status(400).json(e);
   }
 });
@@ -61,8 +50,8 @@ userRoutes.delete('/user/:id', deactivateUser);
 userRoutes.get('/user/active', getAllActiveUsers);
 
 
-userRoutes.get('/test', async(req,res)=>{
-  res.json({message: 'pass!',});
+userRoutes.get('/test', async (req, res) => {
+  res.json({ message: 'pass!', });
 });
 
 function handleSignin(req, res) {
@@ -83,10 +72,10 @@ function handleSignup(req, res) {
 }
 
 
-async function getAllActiveUsers(req, res){
+async function getAllActiveUsers(req, res) {
   let userModel = new Model(userSchema);
   let userList = await userModel.getActive();
-  userList.forEach( user => {
+  userList.forEach(user => {
     delete user.password;
   });
   res.status(200).json(userList);
@@ -96,7 +85,7 @@ async function getAllActiveUsers(req, res){
 async function getAllUsers(req, res) {
   const userList = await users.get();
 
-  userList.forEach( user => {
+  userList.forEach(user => {
     delete user.password;
   });
   res.status(200).json(userList);
@@ -112,13 +101,13 @@ async function getUserById(req, res) {
     });
 }
 
-async function createUser(req, res){
-  try{
+async function createUser(req, res) {
+  try {
     let stored = await users.create(req.body);
     stored = stored.toObject(); // to delete parameters off of a return, must cast `toObject()` to use `delete`
     delete stored.password;
     res.status(201).json(stored);
-  }catch(e){
+  } catch (e) {
     res.status(406).json(e);
   }
 }
@@ -130,10 +119,10 @@ async function updateUser(req, res) {
 }
 
 async function deactivateUser(req, res) {
-  users.update(req.params.id, {'active':false,});
+  users.update(req.params.id, { 'active': false, });
   let itemModel = new Model(itemSchema);
-  itemModel.find({'_custodyId':req.params.id,'owner':req.params.id,}).populate({path:'_owner', select:'_id',})
-    .populate({path:'_custodyId', select:'_id',});
+  itemModel.find({ '_custodyId': req.params.id, 'owner': req.params.id, }).populate({ path: '_owner', select: '_id', })
+    .populate({ path: '_custodyId', select: '_id', });
   // send some message back
   res.send('Your account is successfully deactivated!');
 }
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const { start, } = require('./backend/lib/server.js');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const { start } = require("./backend/lib/server.js");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-mongoose.connect(MONGODB_URL, options);
+mongoose.connect(MONGODB_URI, options);
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', () => console.log('mongodb is connected!'));
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => console.log("mongodb is connected!"));
 
 start();
